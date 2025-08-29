7449a411f865f91cf6f7eceaeddf310fae4ff526
1pjmcdonough <1pjmcdonough@gmail.com>
2025-08-28 23:18:23 -0400

    updating tailor resume sys prompt and adding helpers

--- src/helpers.py
+++ src/helpers.py
@@ -9,10 +9,19 @@ import os
 dotenv_path = Path( Path(__file__).parent.parent, ".env" )
 load_dotenv(dotenv_path=dotenv_path)
 
+
 @dataclass
 class Prompt:
     sys_prompt: str
     usr_prompt: str
+
+
+@dataclass
+class UserInfo:
+    resume: str
+    job_desc: str
+    model: str
+    usr_context: str
 
 
 class Model(Enum):
@@ -21,12 +30,12 @@ class Model(Enum):
 
 
 def set_client(model: str) -> OpenAI:
-    if model == Model.GROK4.name:
+    if model == Model.GROK4.value:
         return OpenAI(
             api_key=os.getenv("XAI_API_KEY"),
             base_url="https://api.x.ai/v1"
         )
-    elif model == Model.GPT4o.name:
+    elif model == Model.GPT4o.value:
         return OpenAI(
             api_key=os.getenv("OPENAI_API_KEY"),
             base_url="https://api.openai.com/v1"
